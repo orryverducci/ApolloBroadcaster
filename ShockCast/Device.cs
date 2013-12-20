f581f20a0732a401c4c47fc7731a1badffe4269e
Orry Verducci <orry@orryverducci.co.uk>
2013-12-20 21:37:05 +0000

    Add inputs base, with list of inputs

---
+++ ShockCast/Device.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShockCast
+{
+    /// <summary>
+    /// An audio device
+    /// </summary>
+    class Device
+    {
+        /// <summary>
+        /// Device name
+        /// </summary>
+        private string name;
+        /// <summary>
+        /// Device ID
+        /// </summary>
+        private string id;
+
+        /// <summary>
+        /// Device name
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        /// <summary>
+        /// Device ID
+        /// </summary>
+        public string ID
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        /// <summary>
+        /// Device information
+        /// </summary>
+        /// <param name="deviceName">The name of the device</param>
+        /// <param name="deviceID">The ID of the device</param>
+        public Device(string deviceName, string deviceID)
+        {
+            name = deviceName;
+            id = deviceID;
+        }
+    }
+}
---
+++ ShockCast/Input.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio;
+using NAudio.CoreAudioApi;
+
+namespace ShockCast
+{
+    /// <summary>
+    /// Audio input
+    /// </summary>
+    class Input
+    {
+        /// <summary>
+        /// Returns a list of all the currently available devices
+        /// </summary>
+        public static List<Device> GetDevices()
+        {
+            // Create the list to return
+            List<Device> devices = new List<Device>();
+            // Create the device enumerator
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            // Enumerate all the plugged in input devices and add them to the list
+            foreach (MMDevice endPoint in enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active))
+            {
+                Device device = new Device(endPoint.FriendlyName, endPoint.ID);
+                devices.Add(device);
+            }
+            // Return the list
+            return devices;
+        }
+    }
+}
