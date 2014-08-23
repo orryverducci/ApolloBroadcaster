5e534f10774a6f877348229c6b5d264bac0560c9
Orry Verducci <orry@orryverducci.co.uk>
2014-08-23 13:07:01 +0100

    Enhance tab UI

---
+++ ShockCast/UI/MenuButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ShockCast.UI
+{
+    class MenuButton : CheckBox
+    {
+        public MenuButton()
+        {
+            this.Appearance = Appearance.Button;
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+            this.BackColor = Color.Transparent;
+            this.FlatAppearance.MouseOverBackColor = Color.FromArgb((255 / 100 * 10), Color.Black);
+            this.FlatAppearance.CheckedBackColor = Color.FromArgb((255 / 100 * 20), Color.Black);
+        }
+    }
+}
